07e39654375e223bd40bf2545d77ffb874f73c57
Aled's Pro! <pc-user@pc-user-no-MacBook-Pro.local>
2017-03-19 21:26:43 +0900

    updates to view, removed helpers

--- js/view.js
+++ js/view.js
@@ -2,10 +2,6 @@ fetch('js/list.json')
   .then((result) => result.json())
   .then((data) => makeHTML(data));
 
-Handlebars.registerHelper('mainTitle', function() {
-
-});
-
 const makeHTML = (data) => {
   let middleList = Math.floor(data.links.length) / 2;
   data.links.splice(middleList, 0, data.mainTitle);
