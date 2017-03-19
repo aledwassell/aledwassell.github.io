f1c73101371b5fcfc90238578b45dd1e671c4e05
Aled's Pro! <pc-user@pc-user-no-MacBook-Pro.local>
2017-03-19 09:35:52 +0900

    finalized popup and implemented the magnific popup

--- js/pop.js
+++ js/pop.js
@@ -1,9 +1,11 @@
-const popUp = {};
-
-popUp.import image from ('image directory');
-
-let popUp.target = document.getElementById('popUp');
-
-popUp.promise('image')
-  .then(magnificPopup({type: 'image'}))
-  .then(this.target.append())
+$(document).ready(function(){
+	$('.open-popup').magnificPopup({
+  type:'inline',
+  midClick: true,
+	zoom: {
+		enabled: true,
+		duration: 600,
+		easing: 'ease-in-out'
+	}
+	});
+});
