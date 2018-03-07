ad45c389aacc44af2bb922d30a57a980f14b33b6
aledwassell <aledwassell@googlemail.com>
2018-03-07 22:49:07 +0000

    implementing app using Angular js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 let port = process.env.PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
