732ea1e5ad4935f4ead56c2a9c05fd880260ff51
aledwassell <aledwassell@googlemail.com>
2018-03-11 08:05:00 +0000

    error less implementation of angular js, but not working

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,17 @@
 (function () {
     'use strict';
     let aledwassellUK = angular.module('aledwassellUK', ['ngResource'])
+        .factory('getList', function ($resource) {
+            return $resource('/list.json', {}, {
+                get: {
+                    method: 'GET'
+                }
+            })
+        })
         .controller('mainRepresentationContoller', ['$scope', function ($scope) {
             $scope.name = 'aled / wassell'
+        }])
+        .controller('listController', ['$scope', 'getList', function ($scope, getList) {
+            console.log(getList.get())
         }])
 })();
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
 let port = process.env.PORT = 3000;
 
 app.use(express.static(__dirname + '/public'));
